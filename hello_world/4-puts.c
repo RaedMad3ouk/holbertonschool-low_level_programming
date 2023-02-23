9239745b447fe1abc46e0f5705180f2019305eec
raed <raed.da3k@gmail.com>
2023-02-23 10:22:58 +0100

    my first c script eveeer

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - touka yarhem weldih 
+* main - touka yarhem weldih
 *
 * Return: Always 0  (Success)
 */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+    return (0);
 }
