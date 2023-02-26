0bc212690d0aea34c26aaccf4187b85f48430ebb
raed <raed.da3k@gmail.com>
2023-02-26 10:10:35 +0100

    print numbers

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main -Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+{
+		putchar(i);
+		i++;
+}
+	putchar ('\n');
+	return (0);
+}
