4d4655434032fd4dc8f85f78e6f716bd87be7d35
raed <raed.da3k@gmail.com>
2023-02-26 10:14:37 +0100

    reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+/**
+ * main - Entry point
+ *
+ * Description: Prints the lowercase alphabet in reverse
+ * using putchar function only twice
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int main(void)
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
