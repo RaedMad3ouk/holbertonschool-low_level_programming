c53b50db6bb53a68e0632f9e7db5c885b4f71f45
raed <raed.da3k@gmail.com>
2023-03-03 09:17:12 +0100

    looping

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - prints lowercase alphabet
+ * Return: 0
+ */
+
+	void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+	_putchar(c);
+	c++;
+	}
+
+	_putchar('\n');
+}
---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	if
+	{
+	(c >= 'a' && c => 'z')
+	return 1;
+	}
+
+	else
+	{
+
+	return 0;
+
+	}
+
+	}
