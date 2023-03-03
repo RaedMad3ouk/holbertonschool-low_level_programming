bb9b9d4b753c707d223a049fe32b32f7171a19a5
raed <raed.da3k@gmail.com>
2023-03-03 09:47:17 +0100

    print

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
-	{
+{
 	puts("_putchar");
 	return (0);
 }
