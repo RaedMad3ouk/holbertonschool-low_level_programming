bdcc84ee6db671f8dd7e58316640d81d6cf77aa6
raed <raed.da3k@gmail.com>
2023-03-05 09:39:12 +0100

    absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * _abs - Absolute value cmnd
+ *
+ * @num: int
+ * Return: absolute value
+ */
+
+int _abs(int num)
+{
+if (num < 0)
+{
+return (-num);
+}
+
+else
+{
+return (num);
+}
+
+
+
+}
