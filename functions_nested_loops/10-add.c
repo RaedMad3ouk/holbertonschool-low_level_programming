b7f47488d3431c0397eb6cf9d13a7260722dbf71
raed <raed.da3k@gmail.com>
2023-03-06 11:54:53 +0100

    jama3

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *add - jama3
+ * Return: 0
+ * @wa7ed: first int
+ * @ethnen: second int
+ * return: wa7ed + ethnen
+ */
+
+int add(int wa7ed, int ethnen)
+{
+
+return (wa7ed + ethnen);
+
+}
