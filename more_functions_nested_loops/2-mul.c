21d801dc7f44133fb269d5429f9945b778099646
raed <raed.da3k@gmail.com>
2023-03-07 13:23:39 +0100

    multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+ #include "main.h"
+
+/**
+ * _isdigit - check for digit
+ * @c: int
+ * Return: 1 if c is digit else return 0
+ */
+
+int mul(int a, int b)
+{
+int r;
+
+r = a * b;
+
+return (r);
+
+}
