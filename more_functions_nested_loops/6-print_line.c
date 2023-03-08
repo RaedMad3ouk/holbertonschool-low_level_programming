5f5a10e656f86a8b38743c367bcf0464468be87c
raed <raed.da3k@gmail.com>
2023-03-08 10:58:54 +0100

    line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_line - a function that draws a line
+* @n: int
+* Return: line
+*/
+
+void print_line(int n)
+{
+int i
+for (i = 0; n > i; i++)
+
+_putchar('_');
+_putchar('\n');
+
+
+
+
+
+}
