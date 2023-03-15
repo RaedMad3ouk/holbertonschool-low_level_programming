40ae49338d0f2adae3bb090da8ecdaac3e6958f3
raed <raed.da3k@gmail.com>
2023-03-15 10:40:46 +0100

    reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: char
+ * Return: reversed string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int n = strlen(s);
+		for (i = n; i >= 0 ; i--)
+	{
+			_putchar(s[i]);
+	}
+	_putchar('\n');
+}
