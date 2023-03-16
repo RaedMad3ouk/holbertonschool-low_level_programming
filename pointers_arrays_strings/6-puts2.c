c6d81d38cc4aed6ba9eb20328faf82d59b59a323
raed <raed.da3k@gmail.com>
2023-03-16 11:20:22 +0100

    add

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] % 2 == 0)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
+}
