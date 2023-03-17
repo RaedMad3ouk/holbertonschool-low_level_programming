1f11e37ce77a2f7f8416d954d6ac8ed15f2b853e
raed <raed.da3k@gmail.com>
2023-03-17 11:35:19 +0100

    number of elements

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
 * print_array - prints number of elements
 * @a: array
@@ -10,14 +9,13 @@
 
 void print_array(int *a, int n)
 {
+	int j;
 
-int i;
-for (i = 0; i < n; i++)
-{
-	printf("%i", a[i]);
-	if (i != n - 1)
-	printf("; ");
-}
+	for (j = 0; j < n; j++)
+	{
+		printf("%d", a[j]);
+		if (j != n - 1)
+			printf(", ");
+	}
 	printf("\n");
-
 }
