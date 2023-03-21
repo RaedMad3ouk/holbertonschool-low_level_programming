1a3869feb5a3fe72487d1ac2a701e844c114f463
raed <raed.da3k@gmail.com>
2023-03-21 09:31:48 +0100

    stringconcat

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: int array
+ * @n: number of elements in the array
+ * Return: concatenated string
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, raed;
+
+
+	for (i = 0; i < n; i++)
+	{
+		raed = a[i];
+		a[i] = a[n];
+		a[n] = raed;
+		n--;
+	}
+}
