6547db1ad59c4bc97379a27a5aa8dd58dd3371d3
raed <raed.da3k@gmail.com>
2023-03-21 09:34:44 +0100

    fixing

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,14 +9,13 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, raed;
+	int i;
+	int raed;
 
-
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n--; i++)
 	{
 		raed = a[i];
 		a[i] = a[n];
 		a[n] = raed;
-
 	}
 }
