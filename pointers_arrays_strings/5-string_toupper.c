d6fe8ed5a3ef5b339c406d7fbe8750e21c255dd7
raed <raed.da3k@gmail.com>
2023-03-21 09:40:53 +0100

    fixing betty

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
- * string_toupper - change all lowercase to uppercase
- * @n: pointer
- * Return: 0
+ * string_toupper - convert to uppercase
+ * @a: string in lowercase
+ * Return: uppercase
  */
 
 char *string_toupper(char *a)
