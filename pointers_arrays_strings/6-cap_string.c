a53e805f8b44f820b4a0067c0927324b5f7e3202
raed <raed.da3k@gmail.com>
2023-03-23 11:52:06 +0100

    add

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * cap_string - capitalize words
  * @a: string
@@ -8,24 +8,26 @@
 
 char *cap_string(char *a)
 {
-	int i, j;
+	int i = 0, j = 0, foundSeparator = 0;
 	char *seperators = ",;.!?\"(){} \n\t";
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-
-		if (a[i] >= 'a' && a[i] <= 'z')
+		for (j = 0; seperators[j] != '\0'; j++)
 		{
-			if (i == 0)
-				a[i] -= 32;
-
-			for (j = 0; seperators[j] != '\0'; j++)
+			if (a[i] == seperators[j] && !(a[i + 1] >= '0' && a[i + 1] <= '9'))
 			{
-				if (a[i] - 1 == seperators[j])
-				{
-					a[i] -= 32;
-				}
+				foundSeparator = 1;
 			}
+		}
+		if (foundSeparator == 1 && a[i] >= 'a' && a[i] <= 'z')
+		{
+			a[i] -= 32;
+			foundSeparator = 0;
+		}
+		else if (foundSeparator == 1 && a[i] >= 'A' && a[i] <= 'Z')
+		{
+			foundSeparator = 0;
 		}
 	}
 	return (a);
