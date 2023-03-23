831f94f46ee739a1a69ddba0dcfd1b0120e976fe
raed <raed.da3k@gmail.com>
2023-03-23 11:36:27 +0100

    update

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,22 +11,22 @@ char *cap_string(char *a)
 	int i, j;
 	char *seperators = ",;.!?\"(){} \n\t";
 
-	for (i = 0; *(a + i) != '\0'; i++)
+	for (i = 0; a[i] != '\0'; i++)
 	{
-		if (*(a + i) >= 'a' && *(a + i) <= 'z')
-			for (j = 0; *(seperators + j) != '\0'; j++)
+
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			if (i == 0)
+				a[i] -= 32;
+
+			for (j = 0; seperators[j] != '\0'; j++)
 			{
-				if (*(a + i - 1) == *(seperators + j))
+				if (a[i] - 1 == seperators[j])
 				{
-					*(a + i) -= 32;
-
-				}
-				else if (i == 0)
-				{
-					*(a + i) -= 32;
-
+					a[i] -= 32;
 				}
 			}
+		}
 	}
 	return (a);
 }
