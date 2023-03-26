b854cb37e0b39a715163f798f75c879d0dab16b2
raed <raed.da3k@gmail.com>
2023-03-26 11:30:58 +0100

    chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -10,10 +10,10 @@ void print_chessboard(char (*a)[8])
 	int j = 0;
 	int i = 0;
 
-	for (j = 0; j < 8; j++) 
+	for (j = 0; j < 8; j++)
 	{
 		_putchar(a[i][j]);
-		
+
 		if (j == 7 && i < 7)
 		{
 			i++;
