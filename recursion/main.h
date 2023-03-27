a3984003689a9bf410babe491e003325b774bf3b
raed <raed.da3k@gmail.com>
2023-03-27 11:56:47 +0100

    update

--- recursion/main.h
+++ recursion/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-raed_sqrt(int n, int x);
+int raed_sqrt(int n, int x);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
