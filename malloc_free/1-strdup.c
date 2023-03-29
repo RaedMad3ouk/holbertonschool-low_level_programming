973d489487f2b01948e79c313595cb98e1405b90
raed <raed.da3k@gmail.com>
2023-03-29 12:01:40 +0100

    cpy a given string

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - copy the string given
+ * @str: string to copy
+ * Return: returns a pointer to the copy, or NULL if it fails
+ */
+
+char *_strdup(char *str)
+{
+unsigned int i, len;
+char *x;
+
+if (!str)
+	return (NULL);
+
+for (len = 0; str[len]; len++)
+
+	len++;
+	x = malloc(len * sizeof(char));
+
+if (!x)
+	return (NULL);
+
+	for (i = 0; i <= len; i++)
+		x[i] = str[i];
+
+	return (x);
+}
