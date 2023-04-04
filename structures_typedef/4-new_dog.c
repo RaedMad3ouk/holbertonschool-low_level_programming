02c328dd33f95f68ae693abb43fbcd4cd47bbb0f
raed <raed.da3k@gmail.com>
2023-04-04 11:12:45 +0100

    add

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+dog_t *x;
+char *newname, *newowner;
+int i, j, k;
+
+x = malloc(sizeof(dog_t));
+	if (!x)
+		return (NULL);
+
+for (i = 0; name[i]; i++)
+	;
+for (j = 0; owner[j]; j++)
+	;
+i++;
+j++;
+newname = malloc(i);
+if (!newname)
+	{
+		free (x);
+		return(NULL);
+	}
+newowner = malloc(j);
+if (!newowner)
+	{
+		free(newname);
+		free (x);
+		return(NULL);
+	}
+	
+	for (k = 0; k < i; k++)
+		newname[k] = name[k];
+	for (k = 0; k < j; k++)
+		newowner[k] = owner[k];
+
+	x->name = newname;
+	x->owner = newowner;
+	x->age = age;
+
+	return (x);
+}
