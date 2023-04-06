343b5e9ceded378e579447d2c5464ff4deea63f2
raed <raed.da3k@gmail.com>
2023-04-06 10:32:46 +0100

    main

---
+++ function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * main - calculator
+ * @argc: size of argv
+ * @argv: array of arguments
+ * Return: answer
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+
+
+
+
+
+
+}
+	
