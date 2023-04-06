8fc2f8835acbbdfc713aec4aaa6a960dd401b9b5
raed <raed.da3k@gmail.com>
2023-04-06 10:52:11 +0100

    add

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -10,17 +10,31 @@
 
 int main(int argc, char *argv[])
 {
+	int i, j;
+	int (*func)(int, int);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
+	i = atoi(argv[1]);
+	j = atoi(argv[3]);
+	func = get_op_func(argv[2]);
 
-
-
-
-
-
+	if (get_op_func(argv[2]))
+		printf("%d\n", get_op_func(func), (i), (j));
+	else
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	return (0);
 }
-	
+
+
+
+
+
+
