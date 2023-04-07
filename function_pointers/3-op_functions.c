c7dcf8b4814f4c2a9a3edcfb312bf447fd1928c0
raed <raed.da3k@gmail.com>
2023-04-07 09:43:02 +0100

    upgrade

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -47,7 +47,13 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+			exit(100);
+	}
 	return ((a) / (b));
+
 }
 
 /**
@@ -59,5 +65,11 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+			exit(100);
+	}
 		return ((a) % (b));
+
 }
