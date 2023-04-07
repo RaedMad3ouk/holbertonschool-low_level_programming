e5e44cb65f87e5debecbfcb09e5c2fc8aba9c161
raed <raed.da3k@gmail.com>
2023-04-07 09:33:10 +0100

    add

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -13,51 +13,60 @@ int op_add(int a, int b)
 {
 	return ((a) + (b));
 }
+
 /**
  * op_sub - difference of a and b
  * @a: integer
  * @b: integer
  * Return: difference if succesful
  */
+
 int op_sub(int a, int b)
 {
 	return ((a) - (b));
 }
+
 /**
  * op_mul - multiply a and b
  * @a: integer
  * @b: integer
  * Return: multiplication of a and b if succesful
  */
+
 int op_mul(int a, int b)
 {
 	return ((a) * (b));
 }
+
 /**
  * op_div - divides a and b
  * @a: integer
  * @b: integer
  * Return: division if succesful
  */
+
 int op_div(int a, int b)
 {
 	if (b == 0)
-	{
+
+	/*{
 		printf("Error\n");
 		exit(100);
-	}
+	}*/
 	return ((a) / (b));
 }
+
 /**
  * op_mod - Module of a and b
  * @a: integer
  * @b: integer
  * Return: remainder if succesful
  */
+
 int op_mod(int a, int b)
 {
 	if (b)
 		return ((a) % (b));
-	printf("Error\n");
-	exit(100);
+	/*printf("Error\n");
+	exit(100);*/
 }
